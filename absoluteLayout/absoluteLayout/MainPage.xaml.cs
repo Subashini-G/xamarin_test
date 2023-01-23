88d23de943a4d1e6fd71139d72f30e30629ebd9a
Subashini-G <subashinig2702@gmail.com>
2023-01-23 10:06:11 +0530

    check commit

---
+++ absoluteLayout/absoluteLayout/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace absoluteLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var layout = new AbsoluteLayout();
+            Content = layout;
+            var aquBox = new BoxView { Color = Color.Gray };
+            layout.Children.Add(aquBox, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(layout, new Rectangle(0.5, 0.1, 100, 100));
+            AbsoluteLayout.SetLayoutFlags(layout, AbsoluteLayoutFlags.HeightProportional);
+        }
+    }
+}
+
